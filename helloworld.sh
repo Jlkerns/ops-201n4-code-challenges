467e48da5ca823fc11226e19f3392a5392ed6da5
Jesse Kerns <jess.l.kerns@gmail.com>
2023-04-26 23:14:49 +0000

    helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+#Script Name: helloworld
+#Author: Jesse Kerns
+#Date of latest revision: 04/26/2023
+#Purpose: First Bash Script
+
+#Declaration of variables
+
+#Declaration of functions
+
+
+#Main
+
+var1="Hellow World"
+echo
+var2="I am working perfectly"
+
+echo $var1 
+
+echo $var2
+
+#End
