75245c57b11de9539b866cbc37f2f388b5da7e67
Jlkerns <jess.l.kerns@gmail.com>
2023-05-09 19:26:31 -0500

    Powershell script

---
+++ PowershellOps9.ps1
@@ -0,0 +1,22 @@
+#Script Name: PowershellOps9.ps1
+#Author: Jesse Kerns
+#Date of latest revision: 5/9/2023
+#Purpose: OutPut System Logs in Powershell
+
+
+Task1
+$Begin = Get-Date -Date '5/8/2023 18:00:00'
+$End = Get-Date -Date '5/9/2023 18:00:00'
+Get-EventLog -LogName System -After $Begin -Before $End > C:\Users\admin\Desktop\24log.txt
+
+Task2
+Get-EventLog -LogName System -EntryType Error > C:\Users\admin\Desktop\errorlog.txt
+
+Task3
+Get-EventLog -LogName System -InstanceID 16 
+
+Task4
+Get-EventLog -LogName System -Newest 20
+
+Task5
+$Events Get-EventLog -LogName System -Newest 500 | select -ExpandProperty message
