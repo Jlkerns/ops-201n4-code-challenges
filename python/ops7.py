acab33cd5e263f06709a5d73e7af6c6ceb218f66
Jlkerns <jess.l.kerns@gmail.com>
2023-06-21 19:33:25 -0500

    ops7.py

--- python/ops7.py
+++ python/ops7.py
@@ -4,6 +4,16 @@
 # Date of latest revision 6//23
 # Purpose: Python Guessing Game
 # Main
+# Bob wants to create a guessing number game!
+# Rule 1: Numbers have to be between 1 and 20
+# Rule 2: Program must run until the correct number is guessed
+# Rule 3: When guessed right, print out how many tries to guess the
+# right number. Example: "Yes! You guessed it in ___ guesses."
+# Rule 4: The program will tell you if your number needs to be HIGHER
+# or LOWER
+# until the number is guessed correctly and the program ENDS.
+# Remeber to import the random function
+#Bonus objective can you put it into a loop to make the game end after 3 turns?
 
 import random
 
