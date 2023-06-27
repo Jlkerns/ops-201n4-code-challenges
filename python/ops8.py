0aa3bfc62f9f1e5e0f313a10d8265961e53f49bb
Jlkerns <jess.l.kerns@gmail.com>
2023-06-27 17:49:52 -0500

    ops8.py

---
+++ python/ops8.py
@@ -0,0 +1,22 @@
+# script: Ops 301 Class 08 Ops Challenge
+# Author: Jesse Kerns
+# Date of latest revision 6/26/23
+# Purpose: Python Lists
+# Main
+
+#Create the list using the variable hero
+hero = ["geralt","wolverine", "venom", "spiderman", "thor", "vision", "bucky", "batman", "superman", "goku"]
+
+#print the value of the 4th variable in the list, reminder 0=1st place
+print(hero[3])
+
+#print the value of 6th through 10th variable
+print(hero[5:10])
+
+#replace 6th varaible with deadpool
+hero[6] = 'deadpool'
+
+#print updated list
+print(hero)
+
+#end
